60d77256541eb218d7593058b582383e179eb546
Arshad <mohdarshad12575@gmail.com>
2026-08-06 00:54:35 +0530

    remove direct res message from the controller and set it to services

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,18 @@
 import { Controller,Get,Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+    constructor(private readonly authService: AuthService) {}
+
     @Get()
     userInfo() {
-        return "user infornation below"
+        return this.authService.userInfo()
     }
+
     @Post('register')
     register() {
-        return "successfully registered"
+        return this.authService.register()
     }
 
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    userInfo() {
+        return "user infornation below"
+    }
+
+    register() {
+        return "successfully registered"
+    }
+}
