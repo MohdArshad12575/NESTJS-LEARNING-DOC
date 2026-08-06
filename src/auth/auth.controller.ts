bc4447104282ee68d3a15b55d597e4aada0b3796
Arshad <mohdarshad12575@gmail.com>
2026-08-07 01:23:02 +0530

    added comment for learning purpose

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,6 +3,9 @@ import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+    // this = AuthController instance 
+    // this.authService = AuthService instance 
+    
     constructor(private readonly authService: AuthService) {}
 
     @Get()
